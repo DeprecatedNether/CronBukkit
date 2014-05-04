7df66626bc7f6264866a56876afc21091570ac04
DeprecatedNether <deprecatednether@gmail.com>
2014-05-04 15:34:00 +0200

    Initial commit

---
+++ README.md
@@ -0,0 +1,4 @@
+CronBukkit
+==========
+
+**CronBukkit** is a scheduler for Bukkit (allows commands to be ran automatically at certain times) that is very much like [cron](http://en.wikipedia.org/wiki/Cron) on Unix-like systems.
---
+++ src/pw/deprecatednether/cronbukkit/CronBukkit.java
@@ -0,0 +1,35 @@
+/*
+ * CronBukkit is a Bukkit plugin that implements a scheduler similar to cron.
+ * Copyright (C) 2014  DeprecatedNether
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program. If not, see <http://www.gnu.org/licenses/>.
+ */
+
+package pw.deprecatednether.cronbukkit;
+
+import org.bukkit.plugin.java.JavaPlugin;
+import org.bukkit.scheduler.BukkitTask;
+
+public class CronBukkit extends JavaPlugin {
+
+    private BukkitTask crontask;
+
+    public void onEnable() {
+
+    }
+
+    public void onDisable() {
+
+    }
+}
