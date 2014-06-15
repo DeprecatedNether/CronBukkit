a5270eb3ec20a8e00c6dc166fa45b747a242ae35
DeprecatedNether <deprecatednether@gmail.com>
2014-06-15 13:05:04 +0200

    Check time in Cron and execute command. Still need date checking

--- src/pw/deprecatednether/cronbukkit/CronTask.java
+++ src/pw/deprecatednether/cronbukkit/CronTask.java
@@ -18,11 +18,76 @@
 
 package pw.deprecatednether.cronbukkit;
 
+import org.bukkit.Bukkit;
 import org.bukkit.scheduler.BukkitRunnable;
+
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.Calendar;
+import java.util.GregorianCalendar;
 
 public class CronTask extends BukkitRunnable {
     @Override
     public void run() {
-        CronBukkit.lastrun = System.currentTimeMillis();
+        long now = System.currentTimeMillis();
+        CronBukkit.lastrun = now;
+        Calendar cal = new GregorianCalendar();
+        int minute = (int)(now / 1000 / 60 % 60);
+        int hour = (int)(now / 1000 / 60 / 60 % 24);
+        try {
+            BufferedReader br = new BufferedReader(new FileReader(CronBukkit.cronFile));
+            String ln;
+            while ((ln = br.readLine()) != null) {
+                Cron cron = new Cron(ln);
+                if (cron.getPredefined() != null) {
+                    break; // We don't want to run predefineds in the scheduler.
+                }
+                // Check if it needs to be run now
+                boolean canRun = true;
+
+                // First, check minute
+                String cMinute = cron.getMinute();
+
+                if (cMinute.contains("/")) {
+                    // Get the part after the /, so we get "5" from */5.
+                    int min = Integer.parseInt(cMinute.split("/")[1]);
+                    if (minute % min != 0) {
+                        canRun = false;
+                    }
+                } else if (cMinute.equals("*")) {
+                    // Keep it at its current value. Don't set to false because it CAN run as far as this is concerned,
+                    // but don't change it to true because previous checks may have made it not run.
+                } else if (minute != Integer.parseInt(cMinute)) {
+                    canRun = false;
+                }
+
+                // Check the hour
+                String cHour = cron.getHour();
+
+                if (cHour.contains("/")) {
+                    // Get the part after the /, so we get "5" from */5.
+                    int h = Integer.parseInt(cMinute.split("/")[1]);
+                    if (hour % h != 0) {
+                        canRun = false;
+                    }
+                } else if (cHour.equals("*")) {
+                    // Keep it at its current value. Don't set to false because it CAN run as far as this is concerned,
+                    // but don't change it to true because previous checks may have made it not run.
+                } else if (hour != Integer.parseInt(cHour)) {
+                    canRun = false;
+                }
+
+                // TODO Date checking
+
+                if (canRun) {
+                    Bukkit.getServer().dispatchCommand(Bukkit.getServer().getConsoleSender(), cron.getCommand());
+                }
+            }
+            br.close();
+        } catch (IOException ioe) {
+            CronBukkit.log.warning("An error occurred during Crontab execution");
+            ioe.printStackTrace();
+        }
     }
 }
